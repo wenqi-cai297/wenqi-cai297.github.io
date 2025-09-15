0ee6bbfcb513ff7e71920dc54c53869f049e569a
wenqi-cai297 <wenqicai297@gmail.com>
2025-09-15 11:59:42 +0000

    Deploying to gh-pages from @ wenqi-cai297/wenqi-cai297.github.io@0e291da4bade04bb47ae12b74b402b60cd559037 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,7 @@ ninja.data = [{
         title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://github.com/https://github.com/wenqi-cai297", "_blank");
+          window.open("https://github.com/wenqi-cai297", "_blank");
         },
       },{
         id: 'social-rss',
